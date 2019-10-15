8f6ef49b33a7c40c59531eb9aa117fcebd6bb56c
c-rosca <constantsa.rosca@gmail.com>
2019-10-15 19:48:44 +0200

    Fixed lawtechlab.java

--- Java/lawtechlab.java
+++ Java/lawtechlab.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class lawtechlab {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Please insert your greeting:");
         String greeting = in.next();
-        System.out.println(greeting);
+        for (int i=0; i<greeting.length();i++)
+        {   System.out.println(greeting.charAt(i));
+        }
+
+        
 
 
     }
