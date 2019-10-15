129681cec470448a56544a65cfec1a117202f91b
c-rosca <constantsa.rosca@gmail.com>
2019-10-15 19:27:51 +0200

    Added two hello world messages in Java and python

--- Java/Hexworld.java
+++ Java/Hexworld.java
@@ -8,12 +8,12 @@ public class Hexworld {
     }
     private static String hexConverter(String hexStr) {
         StringBuilder output = new StringBuilder("");
-         
+
         for (int i = 0; i < hexStr.length(); i += 2) {
             String str = hexStr.substring(i, i + 2);
             output.append((char) Integer.parseInt(str, 16));
         }
-         
+
         return output.toString();
     }
 }
